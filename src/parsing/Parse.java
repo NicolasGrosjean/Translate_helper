d58f0c71a45152a878ee098561ccd94fabaecd74
NicolasGrosjean <grosjen@hotmail.fr>
2015-11-22 14:43:57 +0100

    Complete the structure with language
    In the localisation files, the column of the language can be change in
    the file.
    Therefore when we parse we need the code of the languages to identify them
    if the line to indicate their position is present.
    But too the default position if this line is not present.

--- src/parsing/Parse.java
+++ src/parsing/Parse.java
@@ -9,11 +9,37 @@ public class Parse {
 	private LinkedList<File> files;
 
 	/**
-	 * Parse the files in order to store the entries which are not translated 
+	 * Code for the source language in the "localisation" files
+	 */
+	private String sourceLanguage;
+
+	/**
+	 * Default column for the text of the source language
+	 */
+	private int defaultSourceLanguageColumn;
+
+	/**
+	 * Code for the destination language in the "localisation" files
+	 */
+	private String destinationLanguage;
+
+	/**
+	 * Default column for the text of the destination language
+	 */
+	private int defaultDestinationLanguageColumn;
+
+	/**
+	 * Parse the files in order to store the entries which are not translated
 	 * or without source language text
 	 * @param fileNames The list of the names of the files to parse
 	 */
-	public Parse(LinkedList<String> fileNames) {
+	public Parse(LinkedList<String> fileNames, String sourceLanguage,
+			int defaultSourceLanguageColumn, String destinationLanguage,
+			int defaultDestinationLanguageColumn) {
+		this.sourceLanguage = sourceLanguage;
+		this.defaultSourceLanguageColumn = defaultSourceLanguageColumn;
+		this.destinationLanguage = destinationLanguage;
+		this.defaultDestinationLanguageColumn = defaultDestinationLanguageColumn;
 		files = new LinkedList<File>();
 		for (String fileName : fileNames) {
 			files.addLast(parseAFile(fileName));
