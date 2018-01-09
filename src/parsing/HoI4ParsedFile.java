b3d70733680678cedc12cde3f5366fe85541d83c
Nicolas Grosjean <NicolasGrosjean@users.noreply.github.com>
2018-01-09 20:17:52 +0100

    Fix accent modification when saving #2
    The accent were bad readed when we saved.

--- src/parsing/HoI4ParsedFile.java
+++ src/parsing/HoI4ParsedFile.java
@@ -1,10 +1,11 @@
 package parsing;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
@@ -84,7 +85,8 @@ public class HoI4ParsedFile extends TranslatorParsedFile {
 		BufferedReader file = null;
 		StringBuilder builder = new StringBuilder();
 		try {
-			file = new BufferedReader(new FileReader(filePath));
+			file = new BufferedReader(new InputStreamReader(new FileInputStream(filePath),
+					StandardCharsets.UTF_8));
 			String line;
 			int i = 0;
 			while ((line = file.readLine()) != null) {
